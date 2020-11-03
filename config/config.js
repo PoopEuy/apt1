0c79285b6ed5f47da5906e742963db87c2c6a3a0
dickymahfudin <dickymahfudin13@gmail.com>
2020-11-03 15:51:53 +0700

    edit .env

--- config/config.js
+++ config/config.js
@@ -1,28 +1,32 @@
 require("dotenv").config();
-const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME } = process.env;
-
-console.log(DB_NAME);
+const {
+  DB_CONNECTION,
+  DB_HOST,
+  DB_DATABASE,
+  DB_USERNAME,
+  DB_PASSWORD,
+} = process.env;
 
 module.exports = {
   development: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
-    database: DB_NAME,
+    database: DB_DATABASE,
     host: DB_HOST,
-    dialect: "mysql",
+    dialect: DB_CONNECTION,
   },
   test: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: "database_test",
     host: DB_HOST,
-    dialect: "mysql",
+    dialect: DB_CONNECTION,
   },
   production: {
     username: DB_USERNAME,
     password: DB_PASSWORD,
     database: "database_production",
     host: DB_HOST,
-    dialect: "mysql",
+    dialect: DB_CONNECTION,
   },
 };
