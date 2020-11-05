38bf21e7fa7eda2a7f6a8d3dca3e05064c376da6
dickymahfudin <dickymahfudin13@gmail.com>
2020-11-05 14:32:46 +0700

    add migration and model dock cell

---
+++ migrations/20201105063545-create-table-dock-cell.js
@@ -0,0 +1,72 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("dock_cells", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.BIGINT,
+      },
+      dock_1: {
+        type: Sequelize.INTEGER,
+      },
+      dock_2: {
+        type: Sequelize.INTEGER,
+      },
+      dock_3: {
+        type: Sequelize.INTEGER,
+      },
+      dock_4: {
+        type: Sequelize.INTEGER,
+      },
+      dock_5: {
+        type: Sequelize.INTEGER,
+      },
+      dock_6: {
+        type: Sequelize.INTEGER,
+      },
+      dock_7: {
+        type: Sequelize.INTEGER,
+      },
+      dock_8: {
+        type: Sequelize.INTEGER,
+      },
+      dock_9: {
+        type: Sequelize.INTEGER,
+      },
+      dock_10: {
+        type: Sequelize.INTEGER,
+      },
+      dock_11: {
+        type: Sequelize.INTEGER,
+      },
+      dock_12: {
+        type: Sequelize.INTEGER,
+      },
+      dock_13: {
+        type: Sequelize.INTEGER,
+      },
+      dock_14: {
+        type: Sequelize.INTEGER,
+      },
+      dock_15: {
+        type: Sequelize.INTEGER,
+      },
+      dock_16: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("dock_cells");
+  },
+};
---
+++ src/models/dockCell.js
@@ -0,0 +1,48 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class DockCell extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  DockCell.init(
+    {
+      dock_1: DataTypes.INTEGER,
+      dock_2: DataTypes.INTEGER,
+      dock_3: DataTypes.INTEGER,
+      dock_4: DataTypes.INTEGER,
+      dock_5: DataTypes.INTEGER,
+      dock_6: DataTypes.INTEGER,
+      dock_7: DataTypes.INTEGER,
+      dock_8: DataTypes.INTEGER,
+      dock_9: DataTypes.INTEGER,
+      dock_10: DataTypes.INTEGER,
+      dock_11: DataTypes.INTEGER,
+      dock_12: DataTypes.INTEGER,
+      dock_13: DataTypes.INTEGER,
+      dock_14: DataTypes.INTEGER,
+      dock_15: DataTypes.INTEGER,
+      dock_16: DataTypes.INTEGER,
+      createdAt: {
+        type: DataTypes.DATE,
+        field: "created_at",
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: "updated_at",
+      },
+    },
+    {
+      sequelize,
+      modelName: "dockModel",
+      tableName: "dock_cells",
+    }
+  );
+  return DockCell;
+};
