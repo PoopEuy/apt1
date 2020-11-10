aafb1633eee6a8af668e57c9e7f810557000181b
dickymahfudin <dickymahfudin13@gmail.com>
2020-11-10 11:32:48 +0700

    model and migration service Call

---
+++ src/models/serviceCall.js
@@ -0,0 +1,37 @@
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class ServiceCall extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  ServiceCall.init(
+    {
+      nojs_id: DataTypes.INTEGER,
+      open_time: DataTypes.STRING,
+      closed_time: DataTypes.STRING,
+      error: DataTypes.STRING,
+      status: DataTypes.STRING,
+      createdAt: {
+        type: DataTypes.DATE,
+        field: "created_at",
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        field: "updated_at",
+      },
+    },
+    {
+      sequelize,
+      modelName: "serviceCallModel",
+      tableName: "service_calls",
+    }
+  );
+  return ServiceCall;
+};
