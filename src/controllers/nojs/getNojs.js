15d90209022724bbf503e74f2c69f7f5f451b052
dickymahfudin <dickymahfudin13@gmail.com>
2020-11-12 11:25:41 +0700

    get multiple id

--- src/controllers/nojs/getNojs.js
+++ src/controllers/nojs/getNojs.js
@@ -2,14 +2,32 @@ const { nojsUserModel } = require("../../models");
 
 module.exports = async (req, res) => {
   const userIds = req.query.user_ids || req.body.user_ids || [];
+  let sqlOptions = {
+    attributes: [
+      "id",
+      "nojs",
+      "site",
+      "provinsi",
+      "lc",
+      "mitra",
+      "ip",
+      "latitude",
+      "longitude",
+      "id_lvd_vsat",
+      "id_ping",
+      "id_batt_volt",
+      "id_vsat_curr",
+      "id_bts_curr",
+    ],
+  };
 
   if (userIds.length) {
-    sqlOPtions.where = {
+    sqlOptions.where = {
       id: userIds,
     };
   }
   console.log(req.body);
-  const users = await nojsUserModel.findAll();
+  const users = await nojsUserModel.findAll(sqlOptions);
 
   return res.json({
     status: "success",
