20f825c454314c91ee2044015ba7e78d53abe921
dickymahfudin <dickymahfudin13@gmail.com>
2020-11-16 11:11:00 +0700

    simple get nojs

--- src/controllers/nojs/getNojs.js
+++ src/controllers/nojs/getNojs.js
@@ -1,33 +1,9 @@
 const { nojsUserModel } = require("../../models");
 
 module.exports = async (req, res) => {
-  const userIds = req.query.user_ids || req.body.user_ids || [];
-  let sqlOptions = {
-    attributes: [
-      "id",
-      "nojs",
-      "site",
-      "provinsi",
-      "lc",
-      "mitra",
-      "ip",
-      "latitude",
-      "longitude",
-      "id_lvd_vsat",
-      "id_ping",
-      "id_batt_volt",
-      "id_vsat_curr",
-      "id_bts_curr",
-    ],
-  };
+  const id = req.query.nojs_id || req.body.nojs_id || [];
 
-  if (userIds.length) {
-    sqlOptions.where = {
-      id: userIds,
-    };
-  }
-  console.log(req.body);
-  const users = await nojsUserModel.findAll(sqlOptions);
+  const users = await nojsUserModel.findAllId(id);
 
   return res.json({
     status: "success",
