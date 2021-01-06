41b25a43dbd733853a7a750af5b94ebe564c6fa5
dickymahfudin <dickymahfudin13@gmail.com>
2021-01-06 09:14:31 +0700

    get nojs with offset

--- src/controllers/nojs/getNojs.js
+++ src/controllers/nojs/getNojs.js
@@ -1,4 +1,5 @@
 const { nojsUserModel } = require("../../models");
+const { Op } = require("sequelize");
 
 module.exports = async (req, res) => {
   // const id = req.query.nojs_id || req.body.nojs_id || [];
@@ -8,8 +9,9 @@ module.exports = async (req, res) => {
   //   data: users,
   // });
   const query = req.query;
+  const site = req.query.site;
   const attributes = nojsUserModel.attributes();
-  if (query) {
+  if (!site) {
     const temp = { attributes, where: query };
 
     await nojsUserModel
@@ -17,6 +19,32 @@ module.exports = async (req, res) => {
       .then((result) => {
         return res.json({
           status: "success",
+          data: result,
+        });
+      })
+      .catch((err) => {
+        return res.json({
+          status: "error",
+          data: err,
+        });
+      });
+  } else if (site) {
+    const offset = (site.slice(4, 5) - 1) * 25;
+    const temp = {
+      attributes,
+      order: [
+        ["lc", "ASC"],
+        ["nojs", "ASC"],
+      ],
+      offset,
+      limit: 25,
+    };
+    await nojsUserModel
+      .findAll(temp)
+      .then((result) => {
+        return res.json({
+          status: "success",
+          total: result.length,
           data: result,
         });
       })
