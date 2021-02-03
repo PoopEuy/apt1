17336e76faa87397a00497fce0605b3e5b90ad1b
dickymahfudin <dickymahfudin13@gmail.com>
2021-02-03 10:41:53 +0700

    handle update null

--- src/controllers/statusProgram/ceateOrUpdate.js
+++ src/controllers/statusProgram/ceateOrUpdate.js
@@ -24,26 +24,32 @@ module.exports = async (req, res) => {
   });
 
   if (nojs) {
-    await nojs.update({
-      nojs_id,
-      accumulate_energy_service,
-      check_button_service,
-      handle_canbus_service,
-      handle_mosfet_service,
-      handle_mosfet_timer,
-      handle_relay_service,
-      handle_relay_timer,
-      keep_alive_dock_service,
-      keep_alive_dock_timer,
-      mppt_service,
-      mppt_snmp_service,
-      mppt_snmp_timer,
-      store_log_data_service,
-      store_log_data_timer,
-    });
+    if (accumulate_energy_service) {
+      await nojs.update({
+        nojs_id,
+        accumulate_energy_service,
+        check_button_service,
+        handle_canbus_service,
+        handle_mosfet_service,
+        handle_mosfet_timer,
+        handle_relay_service,
+        handle_relay_timer,
+        keep_alive_dock_service,
+        keep_alive_dock_timer,
+        mppt_service,
+        mppt_snmp_service,
+        mppt_snmp_timer,
+        store_log_data_service,
+        store_log_data_timer,
+      });
+      return res.status(202).json({
+        status: "success",
+        message: "data update",
+      });
+    }
     return res.status(202).json({
       status: "success",
-      message: "data update",
+      message: "data null",
     });
   }
 
