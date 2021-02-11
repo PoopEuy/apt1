4038669f0aa86b4dff77405483c96d7c886b4832
dickymahfudin <dickymahfudin13@gmail.com>
2021-02-11 13:44:52 +0700

    typo

--- src/controllers/cuttoff/get.js
+++ src/controllers/cuttoff/get.js
@@ -1,7 +1,24 @@
 const { cutoffModel } = require("../../models");
 
 module.exports = async (req, res) => {
-  cutoffModel
+  const { site } = req.query;
+  if (site) {
+    return cutoffModel
+      .findAll({ where: { site } })
+      .then((result) => {
+        return res.status(200).json({
+          status: "success",
+          data: result,
+        });
+      })
+      .catch((err) => {
+        return res.status(401).json({
+          status: "error",
+          message: err,
+        });
+      });
+  }
+  return cutoffModel
     .findAll()
     .then((result) => {
       return res.status(200).json({
